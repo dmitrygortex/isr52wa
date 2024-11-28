162fdb5d1208838fe7141f2c0cf83242c4056875
dmitrygortex <gortex@internet.ru>
2024-11-28 14:25:08 +0300

    commit 12

--- Main.java
+++ Main.java
@@ -3,5 +3,6 @@ class Main{
         System.out.print("Это добавлено в ветке dev");
         System.out.println("commit 10 dev");
         System.out.println("commit 11 dev");
+        System.out.println("commit 12 dev");
     }
 }
