c150a818f641a8d38b7ec8648820d40fff088ca9
dmitrygortex <gortex@internet.ru>
2024-11-28 14:13:48 +0300

    commit 4

--- Main.java
+++ Main.java
@@ -1,4 +1,8 @@
 class Main{
     public static void main(String[] args) {
-        System.out.print("Это добавлено в ветке main"); }
+        System.out.print("Это добавлено в ветке main");
+        System.out.println("im");
+
+
+    }
 }
